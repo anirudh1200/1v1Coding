3423456ffcf9b90b19fffecb797f9d51f6ee21ae
Anirudh Bhat <anirudh.b.123@gmail.com>
2018-09-06 19:17:49 +0530

    finished content page

--- public/javascripts/contest.js
+++ public/javascripts/contest.js
@@ -1,3 +1,11 @@
 socket.on("start", () => {
     $("#waitingModal").hide();
 });
+
+var language = "c_cpp";
+var select = document.querySelector("select");
+
+$("select").change(function(){
+    language = select.value;
+    editor.session.setMode("ace/mode/" + language);
+});
